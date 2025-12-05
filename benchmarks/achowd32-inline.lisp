f1da9c2ea059bacb3226bef33ff643cc815c19ee
Atharv Chowdhary <atharv_chowdhary@brown.edu>
2025-12-05 13:37:42 -0800

    fixed miscalculation

--- benchmarks/achowd32-inline.lisp
+++ benchmarks/achowd32-inline.lisp
@@ -1,10 +1,10 @@
-(define (sq x) (* x x))
+(define (db x) (+ x x))
 (define (inc x) (add1 x))
 (define (zero x) (- x x))
-(define (mix1 a b c) (inc (+ (+ (sq (inc a))
-                                (sq (inc b)))
-                             (sq (inc c)))))
-(define (mix2 a b c) (zero (sq (+ (inc a) (inc b)))))
+(define (mix1 a b c) (inc (+ (+ (db (inc a))
+                                (db (inc b)))
+                             (db (inc c)))))
+(define (mix2 a b c) (zero (db (+ (inc a) (inc b)))))
 
 (define (looper n a b c)
   (if (= n 0)
@@ -12,4 +12,3 @@
     (+ (+ (mix2 a b c) (mix1 a b c)) (looper (sub1 n) a b c))))
 
 (print (looper 1000 0 0 0))
-
